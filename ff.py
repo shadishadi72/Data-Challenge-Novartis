b180dfc11a723377f4b79b9df1c88cad8e7daa9a
shadishadi72 <38002675+shadishadi72@users.noreply.github.com>
2020-09-08 09:13:19 +0200

    Update ff.py

--- ff.py
+++ ff.py
@@ -1,3 +1,14 @@
+
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "**Chapter 1 – The Machine Learning landscape**\n",
+    "\n",
+    "_This is the code used to generate some of the figures in chapter 1._"
+   ]
+  },
 # To support both python 2 and python 3
 from __future__ import division, print_function, unicode_literals
 
